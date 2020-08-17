34c058fdc0c1732c60fe0e5a771578e380fb32bc
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-17 18:32:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0 
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 == 0 
      "FizzBuzz"
-  elsif int % 3 == 0 
+  elsif num % 3 == 0 
      "Fizz"
-  elsif int % 5 == 0   
+  elsif num % 5 == 0   
      "Buzz"
   else
     nil
