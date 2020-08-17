1ef4820fd2316d698e3a3236f70d3507d77444ea
Margarita Morozova <ritamorozova@yahoo.com>
2020-08-17 18:29:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-  if num % 3 = 0 && num % 5 = 0 
+  if num % 3 == 0 && num % 5 == 0 
     return "FizzBuzz"
-  elsif num % 3 = 0 
+  elsif num % 3 == 0 
      return "Fizz"
-  else   
-  
+  else num % 5 == 0   
+    return "Buzz"
+  end
 end 
